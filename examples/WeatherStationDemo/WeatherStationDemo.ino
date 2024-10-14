f4785e233fe0fa9eab40568c1aa681520197b0ee
andrew.cliffoutlook <andrew.cliff@outlook.com>
2024-10-14 18:53:49 +0100

    Update 14/10/24 2

--- examples/WeatherStationDemo/WeatherStationDemo.ino
+++ examples/WeatherStationDemo/WeatherStationDemo.ino
@@ -529,7 +529,7 @@ void drawHeaderOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
   display->setTextAlignment(TEXT_ALIGN_RIGHT);
   //String temp = String(currentWeather.temp, 1) + (IS_METRIC ? "°C" : "°F");
   //display->drawString(128, 54, temp);
-  display->drawString(128, 54, WiFi.localIP().toString());
+  display->drawString(128, 54, WiFi.RSSI().toString()+ " db");
   display->drawHorizontalLine(0, 52, 128);
 }
 
